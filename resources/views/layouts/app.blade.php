aaf8e95fb02a7ef14977ddd299ed5fb631315f27
Rpj97 <ramosperez2084@hotmail.com>
2018-05-04 18:46:27 -0700

    Add profile templates. Fix main navbar options.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,61 +21,69 @@
 </head>
 
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <header>
+        @guest
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+        @endguest
 
-            <div class="container">
-                @guest
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                    </a>
-                @else
+        @guest
+            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+        @else
 
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                    Home
-                    </a>
-                @endguest
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
-            @guest
-                <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-            @else
-                <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                My Account <span class="caret"></span>
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    @if (Auth::user()->profile)
-                        <a class="dropdown-item" href="{{ route('profile.show', ['user_id' => Auth::user()->id,'profile_id' => Auth::user()->profile->id]) }}">My Profile</a>
-                    @else
-                        <a class="dropdown-item" href="{{ route('profile.create', ['user_id' => Auth::user()->id]) }}">Create Profile</a>
-                    @endif
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
+            <div class="collapse bg-dark navbar-fixed-top" id="navbarHeader">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8 col-md-7 py-4">
+                            <h4 class="text-white">Information</h4>
+                            <p class="text-muted">Add some Questions. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+                        </div>
+                        <div>
+                            <br><br>
+                            <h4 class="text-white">Setting Information</h4>
+                            <ul class="list-unstyled">
+                                <li>
+                                    @if (Auth::user()->profile)
+                                        <a class="text-white" href="{{ route('profile.show', ['user_id' => Auth::user()->id,'profile_id' => Auth::user()->profile->id]) }}">My Profile</a>
+                                    @else
+                                        <a class="text-white" href="{{ route('profile.create', ['user_id' => Auth::user()->id]) }}">Create Profile</a>
+                                    @endif
+                                </li>
+                                <li><a href="#" class="text-white">Search</a></li>
+                                <li><a href="#" class="text-white">Email</a></li>
+                                <li><a href="#" class="text-white">Instagram</a></li>
+                                <li><a href="#" class="text-white">Facebook</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                </li>
-            @endguest
-            </ul>
-        </div>
-    </div>
-        </nav>
+            </div>
+
+            <div class="navbar navbar-dark bg-dark box-shadow">
+                <div class="container d-flex justify-content-between">
+                    <ul class="text-inline">
+                        <a href="{{ url('/') }}" class="navbar-brand d-flex align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
+                                <strong>Home</strong>
+                        </a>
+                        <a class="text-white" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                               document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                        </a>
+                    </ul>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </div>
+        @endguest
+    </header>
     <main class="py-4">
         <div class="col-12">
             @include('flash.error')
@@ -84,7 +92,6 @@
         </div>
         @yield('content')
     </main>
-</div>
 
 </body>
 
